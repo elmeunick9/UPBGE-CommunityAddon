5259324b56888270f39315d8201858d8000c8b0a
elmeunick9 <elmeunick9@gmail.com>
2017-01-04 00:31:01 +0100

    Test with hook

---
+++ make.py
@@ -0,0 +1,28 @@
+import os
+import zipfile
+
+def fileignore(file):
+	list = [".blend1", ".blend2", ".blend3", ".blend4", ".blend5", ".pyc", "ehthumbs.db", "~", "__pycache__"]
+	for l in list:
+		if file.endswith(l): return True
+
+def zipdir(path, ziph):
+	# ziph is zipfile handle
+	for root, dirs, files in os.walk(path):
+		for file in dirs + files:
+			if not fileignore(file): ziph.write(os.path.join(root, file))
+			
+if __name__ == '__main__':
+
+	#data.zip
+	zipf = zipfile.ZipFile('addon/community/data.zip', 'w', zipfile.ZIP_DEFLATED)
+	zipdir('project/', zipf)
+	zipf.write('project.json')
+	zipf.close()
+	
+	#CommunityAddon.zip
+	os.chdir("addon/")
+	zipf = zipfile.ZipFile('CommunityAddon.zip', 'w', zipfile.ZIP_DEFLATED)
+	zipdir('community/', zipf)
+	zipf.close()
+	os.chdir("../")
