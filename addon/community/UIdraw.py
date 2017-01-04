bc578bb1149dd1cff9ca8eec70e75247338a3e34
elmeunick9 <elmeunick9@gmail.com>
2017-01-04 01:55:30 +0100

    170401

--- addon/community/UIdraw.py
+++ addon/community/UIdraw.py
@@ -14,7 +14,7 @@ class ProjectMenu(bpy.types.Menu):
 		self.layout.operator(LoadGameProject.bl_idname, text="Load Game Project", icon='FILE_FOLDER')
 		if utils.project_data != None:
 			self.layout.operator(ExploreProjectDirectory.bl_idname, text="Explore Project Directory", icon='FILE_FOLDER')
-		
+			self.layout.operator(SaveProjectAs.bl_idname, text="Save Project As ...", icon='SAVE_AS')
 		
 def fileMenuDraw(self, context):
 	
--- addon/community/UIoperator.py
+++ addon/community/UIoperator.py
@@ -1,7 +1,7 @@
 from .macro import *
 from . import utils
 
-import bpy, os, shutil, platform, subprocess
+import bpy, os, shutil, platform, subprocess, tempfile, zipfile
 from bpy.props import *
 
 class NewGameProject(bpy.types.Operator):
@@ -62,7 +62,17 @@ class LoadGameProject(bpy.types.Operator):
 		def getMainBlend(path):
 			path = os.path.dirname(path) + os.sep + "project" +  os.sep + "main.blend"
 			if os.path.isfile(path): return path
-		
+			
+		#If we have a zip
+		if os.path.isfile(self.filepath):
+			for x in [".zip", ".tar", ".gztar", ".bztar", ".xztar"]:
+				if self.filepath.endswith(x):
+					tpath = tempfile.gettempdir() + os.sep + os.path.basename(self.filepath)[:-len(x)] + os.sep
+					shutil.unpack_archive(self.filepath, tpath)
+					self.filepath = tpath
+					print("Extracted to: ", tpath)
+			
+		#Once we have a folder...
 		list=[self.filepath, self.filepath + "project.json", os.path.dirname(self.filepath) + os.sep + "project.json", os.path.dirname(self.filepath) + os.sep + "../" + "project.json"]
 		
 		endpath=None
@@ -74,6 +84,63 @@ class LoadGameProject(bpy.types.Operator):
 			return {'CANCELLED'}
 	
 		bpy.ops.wm.open_mainfile(filepath=endpath)
+		return {'FINISHED'}
+	
+	def invoke(self, context, event):
+		wm = context.window_manager
+		wm.fileselect_add(self)
+		return {'RUNNING_MODAL'}
+		
+class SaveProjectAs(bpy.types.Operator):
+	bl_idname = "wm.save_project_as"
+	bl_label = "Save Game Project As ..."
+	bl_options = {'REGISTER'}
+	
+	filepath = StringProperty(subtype='FILE_PATH')
+	
+	def execute(self, context):
+		if os.path.exists(self.filepath):
+			self.report({'ERROR_INVALID_INPUT'}, "Error saving project," + self.filepath + " file or directory already exists.")
+			return {'CANCELLED'}
+		
+		name = os.path.basename(self.filepath) 
+		if name == "":
+			 self.report({'ERROR_INVALID_INPUT'}, "Error saving project, project name can not be empty.")
+			 return {'CANCELLED'}
+		
+		def isZip(path):
+			for x in [".zip", ".tar", ".gztar", ".bztar", ".xztar"]:
+				if path.endswith(x): return True
+		
+		def fileignore(file):
+			list = [".blend1", ".blend2", ".blend3", ".blend4", ".blend5", ".pyc", "ehthumbs.db", "~", "__pycache__"]
+			for l in list:
+				if file.endswith(l): return True
+
+		def zipdir(path, ziph):
+			# ziph is zipfile handle
+			for root, dirs, files in os.walk(path):
+				for file in dirs + files:
+					if not fileignore(file): ziph.write(os.path.join(root, file))
+				
+		tfolder = os.path.dirname(bpy.data.filepath) + os.sep + ".." + os.sep
+		
+		if isZip(self.filepath):
+			dir = os.getcwd()
+			os.chdir(tfolder)
+			zipf = zipfile.ZipFile(self.filepath, 'w', zipfile.ZIP_DEFLATED)
+			zipdir("project/", zipf)
+			zipf.write('project.json')
+			zipf.close()
+			os.chdir(dir)
+			
+		else:
+			shutil.copytree(tfolder, self.filepath)
+		
+			#Open the new blend
+			bpy.ops.wm.open_mainfile(filepath=self.filepath + os.sep + 'project'  + os.sep + 'main.blend')
+			utils.loadProjectFile(self.filepath + os.sep + 'project.json')
+			
 		return {'FINISHED'}
 	
 	def invoke(self, context, event):
@@ -101,9 +168,11 @@ class ExploreProjectDirectory(bpy.types.Operator):
 def register():
 	bpy.utils.register_class(NewGameProject)
 	bpy.utils.register_class(LoadGameProject)
+	bpy.utils.register_class(SaveProjectAs)
 	bpy.utils.register_class(ExploreProjectDirectory)
  
 def unregister():
 	bpy.utils.unregister_class(NewGameProject)
 	bpy.utils.unregister_class(LoadGameProject)
+	bpy.utils.unregister_class(SaveProjectAs)
 	bpy.utils.unregister_class(ExploreProjectDirectory)
